de796a1dec82650028eea6335877131e7353fa33
Lendl Peralta <janlendl@gmail.com>
2021-07-23 01:50:54 +0000

    completed head.js assignment

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+const head = function(array) {
+  if (array === []) {
+    return array;
+  } else {
+    return array[0];
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),);

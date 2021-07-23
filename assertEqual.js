ed36e7285b52c8d2fb24454ed4e96bc86993e71b
Lendl Peralta <janlendl@gmail.com>
2021-07-23 01:07:54 +0000

    minor revisions on assertEqual assignment

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.assert(actual === expected, '🛑🛑🛑 Assertion Failed: %s !== %s',actual, expected);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };

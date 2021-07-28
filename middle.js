621ddc57a5cdbb1786d3b441acf1649b675f6295
Lendl Peralta <janlendl@gmail.com>
2021-07-28 21:41:17 +0000

    made changes using slice function instead of splice to keep the original elements of the array

--- middle.js
+++ middle.js
@@ -25,17 +25,18 @@ const assertArraysEqual = function(array1, array2) {
 
 const middle = function (array) {
   let output = [];
-  let arrLength = array.length;
+  const arrLength = array.length;
   if (arrLength <= 2) {
     return output;
-  } else if (arrLength % 2 == 0) { //even number
-    output = array.splice(((arrLength/2)-1), 2, arrLength/2);
-  } else if (arrLength % 2 != 0) { //odd number
-    output = array.splice(arrLength/2, 1, arrLength/2);
+  } else if (arrLength % 2 === 0) { //even number
+    output = array.slice(((arrLength/2)-1), (arrLength/2)+1);
+  } else { //odd number
+    output = array.slice(arrLength/2, (arrLength/2)+1);
   }
-
   return output;
 };
 
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+//test comparison
+//console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+//console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));

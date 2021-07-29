5de317c8e8fb478dda1084f56d32bf6448eddb33
Lendl Peralta <janlendl@gmail.com>
2021-07-29 21:02:02 +0000

    created if statement to check each letters

--- countLetters.js
+++ countLetters.js
@@ -8,10 +8,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function (sentence) {
+  const letterCount = {};
   for (const letter of sentence) {
     console.log(letter);
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
   }
-
+  return letterCount;
 };
 
 console.log(countLetters('lighthouse in the house'));

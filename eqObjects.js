e7d4f0a27fbc8c9290e2d83d111d2a4466163353
Lendl Peralta <janlendl@gmail.com>
2021-07-30 04:12:48 +0000

    assertion on primitive data types complete. Commented out the console log for now (will return to it later just in case need it for the next assertion. -- refactor will be done once all is complete

--- eqObjects.js
+++ eqObjects.js
@@ -9,22 +9,25 @@ const assertEqual = function(actual, expected) {
 //returns true if both objects have identical keys with identical values
 //returns false otherwise
 const eqObjects = function(object1, object2) {
-  console.log('object 1: ' + Object.keys(object1));
-  console.log('object 2: ' + Object.keys(object2));
+  let result;
+  //console.log('object 1: ' + Object.keys(object1));
+  //console.log('object 2: ' + Object.keys(object2));
   if((Object.keys(object1).length) === (Object.keys(object2).length)){
     for (const keys of (Object.keys(object1))) {
-      console.log(`${object1[keys]} = ${object2[keys]}`)
-      if(object1[keys] === object2[keys]);
+      //console.log(`${object1[keys]} = ${object2[keys]}`);
+      if(object1[keys] === object2[keys]) {
+        result = true;
+      }
     }
   } else {
-    return (false);
+    result = false;
   }
-
+  return result;
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc)); // => false
+assertEqual(eqObjects(ab, abc), false); // => false

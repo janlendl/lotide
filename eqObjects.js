615a9676e3a610ec59753c5dcd5e2a9d4dc49c3e
Lendl Peralta <janlendl@gmail.com>
2021-07-30 03:48:29 +0000

    added if console to check length of both objects, added for of to loop through the keys

--- eqObjects.js
+++ eqObjects.js
@@ -9,8 +9,22 @@ const assertEqual = function(actual, expected) {
 //returns true if both objects have identical keys with identical values
 //returns false otherwise
 const eqObjects = function(object1, object2) {
+  console.log('object 1: ' + Object.keys(object1));
+  console.log('object 2: ' + Object.keys(object2));
+  if((Object.keys(object1).length) === (Object.keys(object2).length)){
+    for (const keys of (Object.keys(object1))) {
+      console.log('keys :' + keys);
+    }
+    return (true);
+  } else {
+    return (false);
+  }
 
 };
 
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba)); // => true
+
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab,abc));
+assertEqual(eqObjects(ab, abc)); // => false

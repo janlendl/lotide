02d5c7bc74619d2b618e9e294d5589191e205605
Lendl Peralta <janlendl@gmail.com>
2021-07-30 01:59:44 +0000

    Created js file for assignment. Added for of loop and used Object.keys to extract the key

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (tvShows) {
+  const genreType = {};
+  for (const genre of (Object.keys(tvShows))) {
+    console.log(genre);
+  }
+
+  return genreType;
+}
+
+//test case
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

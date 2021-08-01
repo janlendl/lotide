107255ad58adde9535dfe362fa3c452222119e83
Lendl Peralta <janlendl@gmail.com>
2021-08-01 21:41:29 +0000

    was able to get some help with a fellow cohort and did some pair programming with him as the navigator while I figure out the important part of the logic of the code

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, value) => {
+  for (const keys in object) {
+    if (value(object[keys])) {
+      return keys;
+    }
+  }
+};
+
+//Test Case:
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma'); // => "noma"

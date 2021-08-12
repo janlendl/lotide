209c66ceabe65e0d447b4433195fde9e5bcf45cb
Lendl Peralta <janlendl@gmail.com>
2021-08-12 04:56:04 +0000

    refacor middleTest.js to incorporate unit testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+describe('#middle', () => {
+  it('returns [5] from [1, 2, 3, 4, 5, 6, 7, 8, 9]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+  });
+
+  it('returns [5, 6] from [1, 2, 3, 4, 5, 6, 7, 8]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+
+  it('returns [ ] from [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
+
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);

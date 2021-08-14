ca6ed79f8d2703e81264b813ad1b7146d541530d
Lendl Peralta <janlendl@gmail.com>
2021-08-14 02:23:52 +0000

    added the rest of the functions

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  countLetters,
+  countOnly
 };

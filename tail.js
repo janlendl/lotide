19b19306cc381bcb85541e0f688b0845bb1354e8
Lendl Peralta <janlendl@gmail.com>
2021-08-14 01:56:39 +0000

    fixed tail function to return blank array

--- tail.js
+++ tail.js
@@ -2,7 +2,7 @@ const assertEqual = require('./assertEqual')
 
 const tail = (array =>  {
   let newArray = array;
-  return (newArray.length <= 1 ? newArray : newArray = array.slice(1));
+  return (newArray.length <= 1 ? [] : newArray = array.slice(1));
   });
 
 module.exports = tail;
